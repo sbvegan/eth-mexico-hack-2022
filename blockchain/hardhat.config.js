ac7f99e597f7e9cd9f86d1fcafbad0c3e029cda2
soyboy <sbvegan@protonmail.com>
2022-08-19 23:33:27 -0700

    adding deploy script

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -1,8 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-truffle5");
 
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.13",
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
--- blockchain/scripts/deploy.js
+++ blockchain/scripts/deploy.js
@@ -1,30 +1,27 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
+const { Framework } = require("@superfluid-finance/sdk-core");
+require("dotenv").config();
 
+// $ yarn hardhat run scripts/deploy.js --network goerli
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  const provider = new hre.ethers.providers.JsonRpcProvider(process.env.GOERLI_URL);
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const sf = await Framework.create({
+    chainId: (await provider.getNetwork()).chainId,
+    provider
+  });
 
-  await lock.deployed();
+  const signers = await hre.ethers.getSigners();
+  const Dictatorship = await hre.ethers.getContractFactory("Dictatorship");
+  const dictatorship = await Dictatorship.deploy(sf.settings.config.hostAddress);
 
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  await dictatorship.deployed();
+
+  console.log("Dictatorship deployed to:", dictatorship.address);
+  // 0xA1e52F22211f53946feDd43a4287Bc47d3B5b376
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
